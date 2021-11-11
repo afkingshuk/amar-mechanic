184d2593cb0ca0b7c41b7c821d2c63674bb4afca
afkingshuk <afkingshuk@gmail.com>
2021-11-11 21:26:47 +0600

    cdn

--- client.php
+++ client.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"></script>
 </head>
 <body>
 <div class="topnav">
